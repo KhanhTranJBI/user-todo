8634aa566a2d50459b2ff379c7bf7a4c35595a14
Khanh Tran <khanht@julep.com>
2021-03-09 08:38:08 -0800

    Handle error case

--- README.md
+++ README.md
@@ -2,10 +2,6 @@
 
 This is a single page application to show a list of users with a todo list underneath each user if clicking on the plus sign button in front of each user. The todo list item will have the checkbox which either completed or not and a title.
 
-## Todos
-
-Implement code to handle error case for fetching data from the API.
-
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import './App.css';
 
@@ -27,7 +27,7 @@ function App() {
       setUsers(newUsers);
   }
 
-  async function fetchUsers() {
+  const fetchUsers = useCallback(async () =>  {
     try {
       const response = await fetch('https://jsonplaceholder.typicode.com/users');
       const users = await response.json();
@@ -43,15 +43,16 @@ function App() {
     } catch (error) {
       setError(error);
     }
-  }
+  }, []);
 
   useEffect(() => {
     fetchUsers();
-  }, []);
+  }, [fetchUsers]);
 
   return (
     <div className="App">
-      {users.map(user => <User 
+      {error && 'There is an error fetch users data'}
+      {!error && users.map(user => <User 
         key={user.id} 
         data={user} 
         handleNameOnSubmit={handleNameOnSubmit} 
